ce973a841c1c2c27b6c1599073ec89748f2f949c
Guy Mesika <guy@coti.io>
2025-03-18 14:14:43 +0200

    changes to run on a local testnet
    stop start
    
    enabled back the internal TXs

---
+++ docker-compose/reset_blockscout_db.sh
@@ -0,0 +1,6 @@
+   sudo rm -rf ./services/dets/
+   sudo rm -rf ./services/logs/
+   sudo rm -rf ./services/redis-data/
+   sudo rm -rf ./services/blockscout-db-data/
+   sudo rm -rf ./services/stats-db-data/
+
